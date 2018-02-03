f645ac07054f4c4d23fdba505da063d1b382ca11
Victor Opanasyuk <opanasyuk.ob@gmail.com>
2018-02-03 18:41:31 +0200

    added aggregate

--- src/main/java/com/example/integration/conf/AwsConfiguration.java
+++ src/main/java/com/example/integration/conf/AwsConfiguration.java
@@ -2,6 +2,7 @@ package com.example.integration.conf;
 
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +12,21 @@ import org.springframework.integration.core.MessageProducer;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.Transformers;
+import org.springframework.integration.jdbc.JdbcMessageHandler;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.PollableChannel;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class AwsConfiguration {
 
     @Autowired
     private AmazonSQSAsync amazonSqs;
+
+    @Autowired
+    private DataSource dataSource;
 
     @Bean
     public PollableChannel inputChannel() {
@@ -43,11 +52,17 @@ public class AwsConfiguration {
 
     @Bean
     public IntegrationFlow awsSqsToInternalFlow1() {
-        return IntegrationFlows.from(this::awsSqsToInternalFlow)
-                .channel(inputChannel())
+        return IntegrationFlows.from(this::inputChannel)
+                .aggregate(a ->
+                        a.correlationStrategy(Message::getPayload)
+                                .releaseStrategy(g -> g.size() > 100)
+                               )
+                .handle(jdbcMessageHandler())
                 .get();
     }
 
-
-
+    @Bean
+    public MessageHandler jdbcMessageHandler(){
+        return new JdbcMessageHandler(dataSource, "insert into");
+    }
 }
